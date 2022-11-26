9804d9a613011ee0e2303beb63a48aac60d3d1a0
valdenio458 <valdeniomartinho@gmail.com>
2022-11-26 09:46:42 -0300

    Aula 20

---
+++ aula20/loop.rb
@@ -0,0 +1,18 @@
+# while freio_acionado?
+#   puts "Manter parado"
+# end 
+
+# puts "Manter parado" while freio_acionado?
+
+# while freio_acionado? do puts "Manter parado" end
+
+# until freio_acionado? do puts "Manter parado" end
+
+# begin
+#   verificar_abs
+#   executar_parada
+# end while freio_acionado?
+
+for i in 1..5 # range
+  puts i
+end
