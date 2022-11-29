4a70877764651501d2a5d2dc9961d6c88cbcb3cc
valdenio458 <valdeniomartinho@gmail.com>
2022-11-29 18:43:18 -0300

    Aula 30 Arrays

---
+++ aula30/array.rb
@@ -0,0 +1,6 @@
+p lista = [1, 2, 3, 4]
+p lista = Array.new
+p lista = %w(conversao de strings para array) # => ["conversao", "de", "strings", "para", "array"]
+p lista.size # => Imprime tamanho do array => 5
+p lista.empty? # => Imprime se o array está vazio => false
+p lista.join ',' # => "conversao,de,strings,para,array"
