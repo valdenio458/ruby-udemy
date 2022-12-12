f230b2fd458a37893b9afb75953f3ee75f5fea2f
valdenio458 <valdeniomartinho@gmail.com>
2022-12-12 17:38:14 -0300

    Aula 40 - Constantes

---
+++ aula40/constantes.rb
@@ -0,0 +1,12 @@
+# Inicia com uma letra maiúscula
+MAX_USUARIOS = 10;
+
+# Seu valor pode ser alterado.
+
+class Carro
+  MODELOS_DISPONIVEIS = ['Model X', 'Model S']
+end 
+
+
+# Acessa os valores sem instanciar 
+puts Carro::MODELOS_DISPONIVEIS
